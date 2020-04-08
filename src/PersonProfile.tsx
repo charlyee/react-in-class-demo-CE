dc667ed17559792995c434e2006c46ddc374edff
Cole Thomas Evans <cevans2@ualberta.ca>
2020-04-08 14:30:13 -0600

    update

--- src/PersonProfile.tsx
+++ src/PersonProfile.tsx
@@ -1,14 +1,18 @@
-import * as React from 'react';
+import * as React from 'react'; //Import statements at the top of the file.
+//Baseline with tsrcc is going to import baseline react from the react node_module.
+// * "wildcard" means "import all" 
 
-export interface IAppProps {
+export interface IPersonProfileProps {
 }
 
-export default class App extends React.Component<IAppProps> {
-  public render() {
-    return (
-      <div>
-        
-      </div>
-    );
-  }
+export default class PersonProfile extends React.Component<IPersonProfileProps> {
+    public render() {
+        return (
+            <React.Fragment>
+                <h1>{personName}</h1>
+                <p>{personBirthday}</p>
+                <p>{personFavePizza}</p>
+            </React.Fragment>
+        );
+    }
 }
