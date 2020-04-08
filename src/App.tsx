1f87376ee5bf8f3c00ddf98d75c0bbaf79e574c1
Cole Thomas Evans <cevans2@ualberta.ca>
2020-04-08 11:37:31 -0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Something from './Something';
 import LikeButton from './LikeButton';
 
+/**
+ * What is a fragment? https://dev.to/tumee/react-fragments-what-why-how-2kh1
+ * Send single "parent" element from each react component.
+ */
 function App() {
 
   return (
-    <React.Fragment>
+    <Fragment>
       <Fragment>
         <Fragment>
           <h1>This is a header!</h1>
@@ -15,6 +19,13 @@ function App() {
           <button className="SpecialButton">I know what a button is!</button>
           <button>Another button!</button>
           <p>Proof!</p>
+          <Button
+            color='red'
+            content='Like'
+            icon='heart'
+            label={{ basic: true, color: 'red', pointing: 'left', content: '2,048' }}
+          />
+
         </Fragment>
         <Fragment>
           <h1>This is a header!</h1>
@@ -24,7 +35,7 @@ function App() {
           <p>Proof!</p>
         </Fragment>
       </Fragment>
-    </React.Fragment>
+    </Fragment>
   );
 
 
